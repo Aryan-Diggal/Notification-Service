90ae94c888fc28d1e41d0a8e76eb2e9e9bb06f9e
Aryan-Diggal <aryandiggal26@gmail.com>
2026-04-29 19:17:44 +0530

    Basic notification Service setup Done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+const Cron = require('./crons/cron');
+
+const app = express();
+
+const ticketRoutes = require('./routes/ticket.routes');
+
+env.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+ticketRoutes(app);
+
+app.get('/', (req, res) => {
+    res.send('Home page for noti service');
+})
+
+app.listen(process.env.PORT, async () => {
+
+    console.log('Notification server started');
+    // sendMail(process.env.EMAIL, process.env.EMAIL_PASS);
+
+    try {
+
+        if(process.env.NODE_ENV == 'production') {
+            await mongoose.connect(process.env.PROD_DB_URL);
+        } else {
+            await mongoose.connect(process.env.DB_URL);
+        }
+        
+        console.log("Successfully connected to mongo");
+
+    } catch (error) {
+
+        console.log(error);
+    }
+
+    Cron.mailerCron();
+});
